866a904fd99fc7ba164a988e5e145f5621da9d6e
yadav-deepakk <deepakyadav53882@gmail.com>
2026-01-28 23:50:22 +0530

    rename: core-java

---
+++ core-java/oops/Abstraction.java
@@ -0,0 +1,15 @@
+package oops;
+
+abstract class Vehicle {
+  void startVehicle() {
+  }
+}
+
+interface Engine {
+  void start();
+}
+
+public class Abstraction {
+  public static void main(String[] args) {
+  }
+}
